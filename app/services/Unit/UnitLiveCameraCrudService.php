cc22b9eabe58bf3c11b5188717ae5b741417dafe
AhmedFarag12 <ahmed.faragg1211@gmail.com>
2025-08-21 15:35:56 +0300

    add unit camera live

---
+++ app/services/Unit/UnitLiveCameraCrudService.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\services\Unit;
+
+use App\Models\UnitLiveCamera;
+use Illuminate\Support\Facades\DB;
+
+class UnitLiveCameraCrudService
+{
+    public function getUnitLiveCameras($unitId)
+    {
+        return UnitLiveCamera::where('unit_id', $unitId)->get();
+    }
+
+    public function createUnitLiveCameras($request)
+    {
+        return DB::transaction(function () use ($request) {
+            foreach ($request['data'] as $item) {
+                UnitLiveCamera::create([
+                    'unit_id' => $request['unit_id'],
+                    'ip_address' => $item['ip_address'],
+                    'camera_link' => $item['camera_link'],
+                ]);
+            }
+        });
+    }
+
+    public function updateUnitLiveCameras($request)
+    {
+        return DB::transaction(function () use ($request) {
+            foreach ($request['data'] as $item) {
+                $camera = UnitLiveCamera::findOrFail($item['id']);
+
+                $camera->update([
+                    'unit_id' => $request['unit_id'] ,
+                    'ip_address' => $item['ip_address'] ?? $camera->ip_address,
+                    'camera_link' => $item['camera_link'] ?? $camera->camera_link,
+                ]);
+            }
+
+            return true;
+        });
+    }
+
+    public function deleteUnitLiveCamera($id)
+    {
+        return DB::transaction(function () use ($id) {
+            $camera = UnitLiveCamera::findOrFail($id);
+            $camera->delete();
+            return true;
+        });
+    }
+}
