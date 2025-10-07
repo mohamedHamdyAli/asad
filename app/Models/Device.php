1c786381526c84f7a9dd4539adbf7bfcd4da3cef
AhmedFarag12 <68521349+AhmedFarag12@users.noreply.github.com>
2025-10-07 14:44:04 +0300

    Implement notifications structure (#49)

---
+++ app/Models/Device.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Device extends Model
+{
+     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'type',
+        'device_id',
+        'device_type',
+        'token',
+        'login',
+        'notify_status',
+        'device_login'
+    ];
+
+        public static function storeDevice($request)
+    {
+        $user = userAuth() != null ? userAuth() : vendorAuth();
+        Device::updateOrCreate([
+            'device_id'         => $request['device_id'],
+            'user_id'           => $user->id,
+            'type'              => $user->role,
+        ], [
+            'device_id'         => $request['device_id'],
+            'device_type'       => $request['device_type'],
+            'token'             => $user->token()->id,
+            'user_id'           => $user->id,
+            'login'             => 'active',
+        ]);
+    }
+}
---
+++ app/Models/Notification.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Notification extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'type',
+        'objectable_id',
+        'objectable_type',
+        'title',
+        'body',
+        'is_seen',
+        'seen_time',
+    ];
+
+
+    protected function casts(): array
+    {
+        return [
+            'is_seen' => 'boolean',
+            'seen_time' => 'datetime',
+        ];
+    }
+
+    public static function getUserNotification()
+    {
+        return self::whereUserId(userAuth()->id)->get();
+    }
+    public static function getvendorNotification()
+    {
+        return self::whereType('vendor')->whereUserId(vendorAuth()->id)->get();
+    }
+    public function objectable()
+    {
+        return $this->morphTo();
+    }
+}
