992a26303fc28a63e422921df74440b6267d810d
ReemRamzy <reemramzym@gmail.com.com>
2025-12-14 18:06:24 +0200

    contact us fixed bugs

---
+++ resources/js/Components/FormField.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <label v-if="label" class="block text-xs text-gray-500 mb-1">{{ label }}</label>
+
+    <div>
+      <slot></slot>
+    </div>
+
+    <p v-if="error" class="text-red-600 text-xs mt-1">{{ error }}</p>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  label: String,
+  error: String
+})
+</script>
