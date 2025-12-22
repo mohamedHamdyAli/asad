2ed3b9bf19d60009aefbbeafbd33a14b342d4f8a
ReemRamzy <reemramzym@gmail.com.com>
2025-12-22 14:16:29 +0200

    build

--- public/build/assets/InputError-Ce55coll.js
+++
@@ -1,1 +0,0 @@
-import{j as s,C as t,f as a,o as r,b as o,t as c}from"./app-DA7oP-vC.js";const n={class:"text-sm text-red-600"},l={__name:"InputError",props:{message:{type:String}},setup(e){return(i,m)=>s((r(),a("div",null,[o("p",n,c(e.message),1)],512)),[[t,e.message]])}};export{l as _};
