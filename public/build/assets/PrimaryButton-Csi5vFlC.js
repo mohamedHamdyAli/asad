24a2cf42cded367a9965e5a7fdb41aa381588d29
ReemRamzy <reemramzym@gmail.com.com>
2025-12-30 10:06:08 +0200

    build

---
+++ public/build/assets/PrimaryButton-Csi5vFlC.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{f as o,o as r,l as s}from"./app-4D-wkT_p.js";const a={},c={class:"bg-black text-white px-3 py-2 rounded hover:bg-gray-700 disabled:opacity-50"};function n(t,l){return r(),o("button",c,[s(t.$slots,"default")])}const i=e(a,[["render",n]]);export{i as P};
