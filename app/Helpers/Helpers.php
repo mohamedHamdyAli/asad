09d7a77b88d72608c82a2bb3e759b4d23b297fb7
AhmedFarag12 <ahmed.faragg1211@gmail.com>
2026-01-08 19:46:40 +0200

    dd

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -156,7 +156,7 @@ if (!function_exists('userAuth')) {
         if ($user != null && $user->role === 'user') {
             return $user;
         }
-        return failReturnMsg('this user is not authenticated or not a user role');
+        return null;
     }
 }
 if (!function_exists('guestAuth')) {
